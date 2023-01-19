563ac999287e3faed0eb7ba7cd9f713e0d7404e3
denOldTimer <kribo@outlook.com>
2023-01-19 19:11:13 +0100

    init

---
+++ html/App/Modules/Site/Config/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+return (object) array(
+
+  ''    => ['lang' => 'en', 'namespace' => 'App\Modules\Site\Controllers', 'controller' => 'home', 'action' => 'index'],
+  '/'   => ['lang' => 'en', 'namespace' => 'App\Modules\Site\Controllers', 'controller' => 'home', 'action' => 'index'],
+
+);
